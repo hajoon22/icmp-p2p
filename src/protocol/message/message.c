21a9da6ab874d54486271d7d0bec9c6abedb8598
hajoon22 <fleshed-stroll84@icloud.com>
2026-06-08 05:18:28 +0900

    Add message parsing functionality in message.c
    Implement message parsing and validation logic

---
+++ src/protocol/message/message.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdint.h>
+#include <string.h>
+
+#include "../../icmp/icmp.h"
+#include "../../peer/peer.h"
+#include "../../monocypher/monocypher.h"
+
+static int next_index = 0;
+static uint8_t last_ids[10];
+
+static const uint8_t admin_pub[32] = {
+    0xd2,  0x46,  0xda,  0x63,
+    0x92,  0x15,  0x3f,  0x01,
+    0xd6,  0x1f,  0xd1,  0xe0,
+    0xba,  0x0f,  0xf3,  0x4d,
+    0x32,  0xda,  0xb8,  0x5c,
+    0xe5,  0x83,  0xc0,  0x51,
+    0x4e,  0xd3,  0xf4,  0x03,
+    0xfa,  0xc0,  0xc9,  0x45
+};
+
+//[type:1][id:1][message:n][expiry:8][siagnature:64]
+void parse_message(int s, struct icmp_echo *rp) {
+    uint8_t id = *(rp->data+1);
+
+    char *message = rp->data+2;
+    size_t message_len = rp->data_len-74;
+
+    uint64_t expiry;
+    memcpy(&expiry, message+message_len, 8);
+    expiry = be64toh(expiry);
+    
+    char *signature = message+message_len+8;
+
+    // check the signature
+    if (crypto_eddsa_check(signature, admin_pub, rp->data, message_len+10) != 0) {
+		return;
+	}
+
+    // check expiry
+    if (expiry < time(NULL)) {
+        return;
+    }
+    
+    for (int i = 0; i < 10; i++) {
+        if (last_ids[i] == id) return;
+    }
+
+    last_ids[next_index] = id;
+    next_index = (next_index+1)%10;
+
+    printf("%.*s\n", (int)message_len, message);
+    broadcast_peers(s, rp->data, rp->data_len);
+}
