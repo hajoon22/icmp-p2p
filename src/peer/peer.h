e4db8e1ef92e9a82ac90386815a1b9655b2c1edd
hajoon22 <fleshed-stroll84@icloud.com>
2026-06-08 05:17:50 +0900

    Add broadcast_peers function declaration

--- src/peer/peer.h
+++ src/peer/peer.h
@@ -29,5 +29,6 @@ void handle_peers(int s, char *pub, char *priv);
 uint8_t free_slots(void);
 uint32_t *get_peers(uint32_t dst, uint8_t count, uint8_t *len);
 int new_peer(char *pub, uint32_t addr, uint8_t free_slots);
+void broadcast_peers(int s, char *data, size_t len);
 
 #endif
