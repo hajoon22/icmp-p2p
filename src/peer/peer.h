d71419768f54b51f2f47f7dcb8c6c9c755d24a25
hajoon22 <fleshed-stroll84@icloud.com>
2026-06-09 13:12:20 +0900

    Update peer struct with trust and source fields
    Added trust score and source fields to peer struct.

--- src/peer/peer.h
+++ src/peer/peer.h
@@ -12,20 +12,23 @@ enum state {
 
 struct peer {
     enum state state;
-
+    
     char pub_key[32]; // public key
-
     uint32_t address; // ip address
+    uint8_t free_slots; // free connection slots
 
+    
     time_t last_sent;
     time_t last_seen;
-    uint8_t free_slots; // free connection slots
+
+    int trust; // trust score
+    uint32_t source; // source of this peer (ipv4)
 };
 
 void handle_peers(int s, char *pub, char *priv);
 uint8_t free_slots(void);
 uint32_t *get_peers(uint32_t dst, uint8_t count, uint8_t *len);
-int new_peer(char *pub, uint32_t addr, uint8_t free_slots);
+int new_peer(char *pub, uint32_t addr, uint8_t free_slots, uint32_t source);
 void broadcast_peers(int s, char *data, size_t len);
 
 #endif
