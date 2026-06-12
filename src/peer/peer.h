714ff74e154d16e3fd293cac76e7b8e1fe2be2a2
hajoon22 <fleshed-stroll84@icloud.com>
2026-06-12 09:27:00 +0900

    Add is_bootstrap field to peer struct
    Added 'is_bootstrap' boolean to peer structure and updated new_peer function signature.

--- src/peer/peer.h
+++ src/peer/peer.h
@@ -2,6 +2,7 @@
 #define PEER_H
 
 #include <stdint.h>
+#include <stdbool.h>
 #include <time.h>
 
 enum state {
@@ -12,7 +13,8 @@ enum state {
 
 struct peer {
     enum state state;
-    
+    bool is_bootstrap;
+
     char pub_key[32]; // public key
     uint32_t address; // ip address
     uint8_t free_slots; // free connection slots
@@ -28,7 +30,7 @@ struct peer {
 void handle_peers(int s, char *pub, char *priv);
 uint8_t free_slots(void);
 uint32_t *get_peers(uint32_t dst, uint8_t count, uint8_t *len);
-int new_peer(char *pub, uint32_t addr, uint8_t free_slots, uint32_t source);
+int new_peer(char *pub, uint32_t addr, uint8_t free_slots, uint32_t source, bool bootstrap);
 void broadcast_peers(int s, uint8_t fanout, char *data, size_t len);
 int peer_trust(uint32_t addr);
 uint8_t unchecked_slots(void);
