1edf240c50c6ff2c25fe98e2a7b62fd4f3d1d3c3
hajoon22 <fleshed-stroll84@icloud.com>
2026-06-14 03:50:03 +0900

    Refactor peer.h for public key type and function updates
    Updated peer structure to use uint8_t for public key and modified function signatures accordingly.

--- src/peer/peer.h
+++ src/peer/peer.h
@@ -6,16 +6,16 @@
 #include <time.h>
 
 enum state {
-    checked,
-    checking,
-    unchecked,
+    checked, // checked peer
+    checking, // sent the lookup request and wating response
+    unchecked, // not checked yet
 };
 
 struct peer {
     enum state state;
     bool is_bootstrap;
 
-    char pub_key[32]; // public key
+    uint8_t pub_key[32]; // public key
     uint32_t address; // ip address
     uint8_t free_slots; // free connection slots
 
@@ -27,11 +27,11 @@ struct peer {
     uint32_t source; // source of this peer (ipv4)
 };
 
-void handle_peers(int s, char *pub, char *priv);
+void handle_peers(int s, uint8_t *pub, uint8_t *priv);
 uint8_t free_slots(void);
 uint32_t *get_peers(uint32_t dst, uint8_t count, uint8_t *len);
-int new_peer(char *pub, uint32_t addr, uint8_t free_slots, uint32_t source, bool bootstrap);
-void broadcast_peers(int s, uint8_t fanout, char *data, size_t len);
+int new_peer(uint8_t *pub, uint32_t addr, uint8_t free_slots, uint32_t source, bool bootstrap);
+void broadcast_peers(int s, uint8_t fanout, uint8_t *data, size_t len);
 int peer_trust(uint32_t addr);
 uint8_t unchecked_slots(void);
 
