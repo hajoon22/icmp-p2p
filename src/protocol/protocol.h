c6c89327497366675672f0b655718c892a2c0065
hajoon22 <fleshed-stroll84@icloud.com>
2026-06-14 03:49:11 +0900

    Change parameter types from char* to uint8_t*

--- src/protocol/protocol.h
+++ src/protocol/protocol.h
@@ -9,10 +9,10 @@ enum {
     MESSAGE = 1,
 };
 
-void parse_lookup_request(int s, char *pub, char *priv, struct icmp_echo *rp);
-int send_lookup_request(int s, char *pub, char *priv, uint32_t addr, uint8_t f);
+void parse_lookup_request(int s, uint8_t *pub, uint8_t *priv, struct icmp_echo *rp);
+int send_lookup_request(int s, uint8_t *pub, uint8_t *priv, uint32_t addr, uint8_t f);
 
-void parse_lookup_response(int s, char *pub, char *priv, struct icmp_echo *rp);
-int send_lookup_response(int s, char *pub, char *priv, uint32_t addr, uint8_t want, uint8_t f);
+void parse_lookup_response(int s, uint8_t *pub, uint8_t *priv, struct icmp_echo *rp);
+int send_lookup_response(int s, uint8_t *pub, uint8_t *priv, uint32_t addr, uint8_t want, uint8_t f);
 
 #endif
