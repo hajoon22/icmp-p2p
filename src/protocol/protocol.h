0973bc107b1cc34f204da8f89f7804cd94eaf453
hajoon22 <hajoon@hajoon.me>
2026-06-17 23:36:25 +0900

    Refactor protocol.h for lookup request/response

--- src/protocol/protocol.h
+++ src/protocol/protocol.h
@@ -5,14 +5,14 @@
 #include "../icmp/icmp.h"
 
 enum {
-    LOOKUP = 0,
-    MESSAGE = 1,
+    LOOKUP_REQ = 0,
+    LOOKUP_RES = 1,
+    MESSAGE = 2,
 };
 
-void parse_lookup_request(int s, uint8_t *pub, uint8_t *priv, struct icmp_echo *rp);
-int send_lookup_request(int s, uint8_t *pub, uint8_t *priv, uint32_t addr, uint8_t f);
+void parse_lookup_request(int s, uint8_t *pub, uint8_t *priv, struct icmp_unreach *rp);
+void send_lookup_request(int s, uint8_t *pub, uint8_t *priv, uint32_t daddr, uint16_t dport, uint16_t sport, uint8_t f);
 
-void parse_lookup_response(int s, uint8_t *pub, uint8_t *priv, struct icmp_echo *rp);
-int send_lookup_response(int s, uint8_t *pub, uint8_t *priv, uint32_t addr, uint8_t want, uint8_t f);
-
+void parse_lookup_response(int s, uint8_t *pub, uint8_t *priv, struct icmp_unreach *rp);
+void send_lookup_response(int s, uint8_t *pub, uint8_t *priv, uint32_t addr, uint16_t port, uint8_t want, uint8_t f);
 #endif
