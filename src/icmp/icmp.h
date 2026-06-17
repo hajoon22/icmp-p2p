6d44f4455cfad0264e049514c77613961c6ca3c3
hajoon22 <hajoon@hajoon.me>
2026-06-17 23:38:04 +0900

    Update ICMP header for unreachable packet handling
    Refactor ICMP structures and functions for unreachable packets.

--- src/icmp/icmp.h
+++ src/icmp/icmp.h
@@ -1,20 +1,14 @@
 #ifndef ICMP_H
 #define ICMP_H
 
-#include <stddef.h>
 #include <stdint.h>
+#include <stddef.h>
 #include <netinet/ip.h>
 #include <netinet/ip_icmp.h>
 
 #define MAX_DATA_BUFFER 1500
 
-enum {
-    ICMP_UNKNOWN_TYPE = -1,
-    ICMP_ERROR_BUILD = -2,
-    ICMP_ERROR_SEND = -3,
-};
-
-struct icmp_echo {
+struct icmp_unreach {
     struct iphdr iph;
     struct icmphdr icmph;
 
@@ -22,8 +16,8 @@ struct icmp_echo {
     size_t data_len;
 };
 
-void deinit_icmp_echo(struct icmp_echo *rp);
-struct icmp_echo *read_icmp_echo(int s);
-int send_echo_packet(int s, uint8_t type, uint32_t dst, uint8_t *data, size_t len);
+void deinit_icmp_unreach(struct icmp_unreach *rp);
+struct icmp_unreach *read_icmp_unreach(int s);
+int send_icmp_unreach(int s, uint32_t saddr, uint16_t sport, uint32_t daddr, uint16_t dport, uint8_t *data, size_t len);
 
 #endif
