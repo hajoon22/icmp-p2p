fc2f9e542e35e10c084fbcc02b981c3d3a43371a
hajoon22 <hajoon@hajoon.me>
2026-06-18 09:12:21 +0900

    Validate IP address and port length in utils.c

--- src/utils/utils.c
+++ src/utils/utils.c
@@ -36,18 +36,20 @@ int parse_ipv4_port(char *arg, uint32_t *ip, uint16_t *port) {
 
     // parse ipv4
     size_t ip_len = c-arg;
-    if (ip_len >= 16 || ip_len == 0) return -1;
+    if (ip_len >= 16 || ip_len == 0) return -1; // invalid ip address length
 
     char ip_str[16];
     memcpy(&ip_str, arg, ip_len);
     ip_str[ip_len] = '\0';
+
     *ip = ntohl(inet_addr(ip_str));
+    if (*ip == 0xFFFFFFFF) return -1; // invalid ip address
 
     // parse port
     char *end;
     unsigned long port_long = strtoul(c+1, &end, 10);
-    if (*end != '\0' || port_long > 65535) {
-        return -1;
+    if (*end != '\0' || port_long > 65535 || port_long < 1) {
+        return -1; // invalid port
     }
     *port = (uint16_t)port_long;
 
