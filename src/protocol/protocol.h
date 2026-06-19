97c6db31c4363cf15da68b857309af1c9a02c273
hajoon22 <hajoon@hajoon.me>
2026-06-19 10:10:15 +0900

    Change send_lookup_request to return uint16_t

--- src/protocol/protocol.h
+++ src/protocol/protocol.h
@@ -11,8 +11,8 @@ enum {
 };
 
 void parse_lookup_request(int s, uint8_t *pub, uint8_t *priv, struct icmp_unreach *rp);
-void send_lookup_request(int s, uint8_t *pub, uint8_t *priv, uint32_t daddr, uint16_t dport, uint16_t sport, uint8_t f);
-
 void parse_lookup_response(int s, uint8_t *pub, uint8_t *priv, struct icmp_unreach *rp);
-void send_lookup_response(int s, uint8_t *pub, uint8_t *priv, uint32_t addr, uint16_t port, uint8_t want, uint8_t f);
+
+uint16_t send_lookup_request(int s, uint8_t *pub, uint8_t *priv, uint32_t daddr, uint16_t dport, uint16_t sport, uint8_t f);
+
 #endif
