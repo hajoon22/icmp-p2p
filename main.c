41d8fbe17aa2e623929bb2254758dca9501b8c3b
hajoon22 <hajoon@hajoon.me>
2026-06-22 13:43:18 +0900

    Refactor main.c to use traversal session

--- main.c
+++ main.c
@@ -9,13 +9,13 @@
 #include <stdbool.h>
 
 #include "config.h"
-#include "src/stun/stun.h"
-#include "src/icmp/icmp.h"
 #include "src/peer/peer.h"
 #include "src/utils/utils.h"
 #include "src/protocol/protocol.h"
 #include "src/monocypher/monocypher.h"
 #include "src/protocol/message/message.h"
+#include "src/traversal/traversal.h"
+#include "src/traversal/icmp/icmp.h"
 
 int main(int argc, char *argv[]) {
 	// generate random seed and public/private key
@@ -24,21 +24,15 @@ int main(int argc, char *argv[]) {
 
 	crypto_eddsa_key_pair(priv, pub, seed);
 
-	// get public address and make mapping port
-	uint32_t pub_addr;
-	uint16_t mapped_port;
-	int udp_s = get_stun(ntohl(inet_addr(STUN_ADDR)), STUN_PORT, &pub_addr, &mapped_port); 
-	if (udp_s < 0) {
-		return -1;
-	}
-
-	struct in_addr a;
-    a.s_addr = htonl(pub_addr);
-	printf("udp mapped: public addr=%s, port=%d\n", inet_ntoa(a), mapped_port);
-
-	// make icmp raw socket
-	int icmp_s = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP);
-	if (icmp_s < 0) return icmp_s;
+    struct traversal_session ts;
+	uint32_t stun_address = ntohl(inet_addr(STUN_ADDR));
+    if (new_traversal_session(&ts, stun_address, STUN_PORT) < 0) {
+        return -1;
+    }
+    
+    struct in_addr a;
+    a.s_addr = htonl(ts.public_address);
+    printf("udp mapped: public addr=%s, public sport=%d\n", inet_ntoa(a), ts.mapped_port);
 
 	// send lookup request packet to bootstraps
 	for (int i = 1; i < argc; i++) {
@@ -52,63 +46,22 @@ int main(int argc, char *argv[]) {
     	a.s_addr = htonl(addr);
 		printf("bootstrap: addr=%s, mapped port=%d\n", inet_ntoa(a), port);
 
-		uint16_t nonce = send_lookup_request(icmp_s, pub, priv, addr, port, mapped_port, free_slots());
+		uint16_t nonce = send_lookup_request(&ts, pub, priv, addr, port, free_slots());
 		new_peer(NULL, addr, port, 0, 0, nonce, 0, true);
 	}
 
-	struct pollfd pfd = {
-		.fd = icmp_s,
-		.events = POLLIN
-	};
-
-	int c = 0;
 	while (1) {
-		c++;
-		if (c == 10) { // 10 sec
-			c = 0;
-			send(udp_s, "aaaaa", 5, 0); // keepalive udp mapping
-		}
+		handle_peers(&ts, pub, priv);
+		struct icmp_unreach *rp = traversal_read(&ts, 100);
+		if (!rp) continue;
 
-		handle_peers(icmp_s, mapped_port, pub, priv);
+		parse_message(&ts, rp);
+		parse_lookup_request(&ts, pub, priv, rp);
+		parse_lookup_response(pub, priv, rp); 		
 
-		int n = poll(&pfd, 1, 100);
-		if (n < 0) break;
-		
-		if (n > 0) {
-			struct icmp_unreach *rp = read_icmp_unreach(icmp_s);
-			if (!rp) continue;
-
-			parse_message(icmp_s, rp);
-			parse_lookup_request(icmp_s, pub, priv, rp);
-			parse_lookup_response(icmp_s, pub, priv, rp); 
-
-			deinit_icmp_unreach(rp);
-		}
+		deinit_icmp_unreach(rp);
 	}
-}
 
-	int c = 0;
-	while (1) {
-		c++;
-		if (c == 10) { // 10 sec
-			c = 0;
-			send(udp_s, "aaaaa", 5, 0); // keepalive udp mapping
-		}
-
-		handle_peers(icmp_s, mapped_port, pub, priv);
-
-		int n = poll(&pfd, 1, 100);
-		if (n < 0) break;
-		
-		if (n > 0) {
-			struct icmp_unreach *rp = read_icmp_unreach(icmp_s);
-			if (!rp) continue;
-
-			parse_message(icmp_s, rp);
-			parse_lookup_request(icmp_s, pub, priv, rp);
-			parse_lookup_response(icmp_s, pub, priv, rp); 
-
-			deinit_icmp_unreach(rp);
-		}
-	}
+	deinit_traversal_session(&ts);
+	return 0;
 }
