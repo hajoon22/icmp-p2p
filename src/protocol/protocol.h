b090c9c92f85536bfaea2772e4ba0f198994a8dc
hajoon22 <hajoon@hajoon.me>
2026-06-22 13:41:52 +0900

    Refactor protocol.h to include traversal functions
    Updated function signatures to use traversal_session struct.

--- src/protocol/protocol.h
+++ src/protocol/protocol.h
@@ -2,7 +2,9 @@
 #define PROTOCOL_H
 
 #include <stdint.h>
-#include "../icmp/icmp.h"
+
+#include "../traversal/traversal.h"
+#include "../traversal/icmp/icmp.h"
 
 enum {
     LOOKUP_REQ = 0,
@@ -10,9 +12,9 @@ enum {
     MESSAGE = 2,
 };
 
-void parse_lookup_request(int s, uint8_t *pub, uint8_t *priv, struct icmp_unreach *rp);
-void parse_lookup_response(int s, uint8_t *pub, uint8_t *priv, struct icmp_unreach *rp);
+void parse_lookup_request(struct traversal_session *ts, uint8_t *pub, uint8_t *priv, struct icmp_unreach *rp);
+void parse_lookup_response(uint8_t *pub, uint8_t *priv, struct icmp_unreach *rp);
 
-uint16_t send_lookup_request(int s, uint8_t *pub, uint8_t *priv, uint32_t daddr, uint16_t dport, uint16_t sport, uint8_t f);
+uint16_t send_lookup_request(struct traversal_session *ts, uint8_t *pub, uint8_t *priv, uint32_t daddr, uint16_t dport, uint8_t f);
 
 #endif
